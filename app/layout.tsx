5e0d0f0835ca883a85dd7daad65a484ee798c0a5
Austin Hem <rap021.sairon@gmail.com>
2026-05-30 19:21:20 +0430

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// @ts-ignore: allow side-effect import of global CSS without explicit type declarations
+import './ui/global.css';
+import { inter } from './ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
